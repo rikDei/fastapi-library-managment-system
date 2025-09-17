9f6a0943bb956ed55269d490c2493baae9b629fe
Riccardo Deidda <riccardo.deidda@accenture.com>
2025-09-17 18:03:02 +0200

    Added loan model.

---
+++ app/models/loan.py
@@ -0,0 +1,33 @@
+from datetime import date
+
+from sqlmodel import Field, SQLModel
+
+
+class LoanBase(SQLModel):
+    book_id: int = Field(nullable=False, foreign_key="book.id")
+    user_id: int = Field(nullable=False, foreign_key="user.id")
+    loan_date: date = Field(
+        nullable=False,
+        default=date.today(),
+        description="The exact date when the book was checked out.",
+    )
+    due_date: date = Field(
+        nullable=False, description="The date by which the book must be returned."
+    )
+    return_date: date | None = Field(
+        nullable=True,
+        default=None,
+        description="The date when the book was actually returned. This field is NULL until the book is checked back in.",
+    )
+
+
+class Loan(LoanBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class LoanPublic(LoanBase):
+    pass
+
+
+class LoanCreate(LoanBase):
+    pass
